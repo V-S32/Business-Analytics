a45791a7e39e160217768bad30d1c09c50a02305
Victoria Smart <svictoria32@outlook.com>
2025-02-13 13:12:57 -0500

    Queries

---
+++ query/preprocessing query.sql
@@ -0,0 +1,17 @@
+/* © 2025 Victoria Smart. All rights reserved. 
+https://v-s32.github.io/Business-Analytics/
+Business-Analytics*/
+
+--Checking all Tables--
+
+--orders table
+SELECT * 
+FROM orders;
+select 
+--products table
+SELECT * 
+FROM products;
+
+--propertyinfo table
+SELECT * 
+FROM propertyinfo;
